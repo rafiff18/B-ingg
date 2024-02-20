d637dfa482e109c1c07a66769a4b7ac30d67be26
rafiff18 <rafiffiko18@gmail.com>
2024-02-20 13:21:03 +0700

    a

--- act 2 descrinbinmg place.py
+++ act 2 descrinbinmg place.py
@@ -1,50 +1,23 @@
 Act 1:
 
-[dilluar bangungan]
+r: morning bro  whats good!!
 
-Anna: Wow, this vocational building looks so majestic, doesn't it, Boba?
+n: morning bro, long time no see!
 
-Boba: Yes,the green trees, and the modern architecture make it look very cool.
+r: are  you ready to explore this campus
 
-Anna: I like how well-maintained the buildings are. It looks so prestigious.
+N: waitt i A Bit Peckish, can we get some snack 
 
-Boba: Absolutely. It feels like stepping into a world of knowledge
+r: sure, pick 1 canteen do you want to go!
 
-Anna: Don't forget, we also need to explore their famous library.
+N: wait can you explain which one is better for snacking?
 
-Boba: Of course! I can'show you to see a lot of books.
+r: i think its difficult because we have 3 canteen here.
 
-Anna: And imagine how the cafeteria must be during breaks. It would be fun to find a seat among all the chatting students.
+n: if you dont mind can we explore all of the canteen.
 
-Boba: Yes,  Let's go in and take a closer look.
+r: 
 
-Act 2:
-
-Anna: letsgo to the library
-
-Boba: waitt a second
-
-Anna: okey lets do a small walk to the library
-
-[Inside the university library]
-
-Anna: Wow, the library is so organized. The books here must be very valuable.
-
-Boba: That's right. I like how the quiet atmosphere to focus on studying.
-
-Anna: Look, there are many students sitting seriously reading and researching.
-
-Boba: It seems like the perfect place to find references for your future assignments.
-
-Anna: Yes, and I also heard that they have access to the latest academic journals.
-
-Boba: That's great. It will help us in writing our papers.
-
-Anna: Let's explore this room even further.
-
-Boba: I agree. There must be a lot of knowledge waiting for us to discover here.
-
-Act 3:
 
 [In the university cafeteria]
 
@@ -73,3 +46,52 @@ Anna: Of course. But for now, I just want to enjoy the atmosphere of this cafete
 Boba: I agree. The life of a student is never boring.
 
 Anna: Absolutely. Let's enjoy this moment before getting back to our routines.
+
+
+[dilluar bangungan]
+
+Anna: Wow, this buildings looks so cool, doesn't it, Boba?
+
+Boba: Yes,the modern architecture make it look very cool.
+
+Anna: I like how well-maintained the buildings are. 
+
+Boba: Absolutely. It feels like stepping into a world of knowledge
+
+Anna: Don't forget, we also need to explore their famous library.
+
+Boba: Of course! I can'show you to see a lot of books.
+
+Anna: And imagine how the cafeteria must be during breaks. It would be fun to find a seat among all the chatting students.
+
+Boba: Yes,  Let's go in and take a closer look.
+
+Act 2:
+
+Anna: do you know where is the library?
+
+Boba: ofcourse i know
+
+Anna: is it upstair?
+
+boba: no,just go ahead , it will be in right site of this hall
+
+[Inside the university library]
+
+Anna: Wow, the library is so organized. The books here must be very valuable.
+
+Boba: That's right. I like how the quiet atmosphere .
+
+Anna: Look, there are many students sitting seriously reading and researching.
+
+Boba: It seems like the perfect place to find references for your future assignments.
+
+Anna: Yes, and I also heard that they have access to the latest academic journals.
+
+Boba: That's great. It will help us in writing our papers.
+
+Anna: Let's explore this room even further.
+
+Boba: I agree. There must be a lot of knowledge waiting for us to discover here.
+
+Act 3:
